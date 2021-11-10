b5d5b2986dd93af3a9a53b9fe0d556f08beba462
annaspi <65283400+annaspi@users.noreply.github.com>
2021-11-10 12:05:28 +0100

    timerfunktion fürs eingabefeld

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,9 @@ export class AppComponent {
   counter = 0;
   Bilder:string[] = ["https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_960_720.jpg", "https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_960_720.jpg"];
 
+  timerison:boolean=false
+  Timer:number=0
+
   onNext(){
     if(this.counter < this.Bilder.length-1){
       this.counter++;
@@ -28,10 +31,21 @@ export class AppComponent {
 
   addUrl(event: Event){
     const url = (<HTMLInputElement>event.target).value;
-    if(url.endsWith(".jpg")){
-      this.Bilder.push(url);
-    }else{
-      console.log("Bitte gültige Url angeben")
+    if (this.timerison){
+      clearTimeout(this.Timer)
     }
+    this.timerison=true
+    this.Timer=setTimeout(()=>{
+      this.timerison=false
+      if(url.endsWith(".jpg")){
+        this.Bilder.push(url);
+      }else{
+        console.log("Bitte gültige Url angeben")
+      }    },5000)
+
+
   }
+
+
+
 }
